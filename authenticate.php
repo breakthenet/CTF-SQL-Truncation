9bb15c4086a744df7ca76d9ee0a8a1be196420d3
emeth- <seanybob@gmail.com>
2017-01-01 06:45:20 -0600

    tweak

--- authenticate.php
+++ authenticate.php
@@ -24,25 +24,18 @@ if (empty($username) || empty($password))
 }
 
 $username = mysql_real_escape_string($username);
-$uq = mysql_query("SELECT `username`, `password` FROM `users` WHERE `username` = '$username'", $c);
+$password = md5($password);
+$uq = mysql_query("SELECT `username`, `password` FROM `users` WHERE `username` = '$username' AND `password` = '$password'", $c);
 if (mysql_num_rows($uq) == 0)
 {
     die(
             "<h3>breakthenet Error</h3>
-	Invalid username!<br />
+	Invalid username or password!<br />
 	<a href='login.php'>&gt; Back</a>");
 }
 else
 {
     $mem = mysql_fetch_assoc($uq);
-    $login_failed = !($mem['password'] === md5($password));
-    if ($login_failed)
-    {
-        die(
-                "<h3>breakthenet Error</h3>
-		Invalid password!<br />
-		<a href='login.php'>&gt; Back</a>");
-    }
     session_regenerate_id();
     $_SESSION['loggedin'] = 1;
     $_SESSION['username'] = $mem['username'];
