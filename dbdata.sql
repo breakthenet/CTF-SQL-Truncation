eadd9797fb7bbc235ab6467705769503347fb553
emeth- <seanybob@gmail.com>
2017-01-01 06:35:53 -0600

    fix

--- dbdata.sql
+++ dbdata.sql
@@ -3,7 +3,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL DEFAULT '',
   `password` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`userid`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` VALUES (1,'admin','e229dca52dbfa50c12f80f746c8d6867');
